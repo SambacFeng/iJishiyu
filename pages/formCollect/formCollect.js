9c63ea4c2a2ff1ca685e0e8503e0abf2b1d75472
SambacFeng <feng20000310@qq.com>
2021-01-26 13:07:51 +0800

    Initial Commit

---
+++ pages/formCollect/formCollect.js
@@ -0,0 +1,73 @@
+// pages/formCollect/formCollect.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: [
+      {key: 1, value: '重装系统', isChecked: false},
+      {key: 2, value: '蓝屏、黑屏', isChecked: false},
+      {key: 3, value: '网络问题，无法连接校园网等', isChecked: false},
+      {key: 4, value: '需安装软件或软件使用过程中遇到问题', isChecked: false},
+      {key: 5, value: '拆机清灰、更换硅脂等', isChecked: false},
+      {key: 6, value: '其他或问题不清楚，请在下方简单描述', isChecked: false},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
