97f52e125a978ddcf65f4420c29ecd6c09909932
疯疯芸 <oncwnuPPfiiE_3gA7MKDbYDTxq-g@git.weixin.qq.com>
2021-01-29 09:47:20 +0800

    接单信息查询

--- cloudfunctions/query/index.js
+++ cloudfunctions/query/index.js
@@ -10,25 +10,32 @@ exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
   const db = cloud.database()
   form = {}
-  usertype = (await db.collection('Member').where({_openid: wxContext.OPENID}).get()).data[0].type
-
-  if (event.type == 'UserQuery'){
+  
+  if (event.type === 'UserQuery'){
     form = (await db.collection('Forms').where({_openid: wxContext.OPENID}).orderBy('_time','desc').get()).data
-  } else if (event.type == 'StaffQuery'){
-    if (usertype != 1){
-      return {}
+  } else {
+    usertype = (await db.collection('Member').where({_openid: wxContext.OPENID}).get()).data[0].type
+    if (event.type === 'StaffQuery'){
+      if (usertype != 1){
+        return {}
+      }
+      form = (await db.collection('Forms').where({_staffopenid: wxContext.OPENID}).orderBy('_time','desc').get()).data
+    } else if (event.type === 'formClaim'){
+      if (usertype != 1){
+        return {}
+      }
+      form = (await db.collection('Forms').where({_staffopenid: ''}).orderBy('_time','desc').get()).data
+    } else if (event.type === 'ManagerMemberQuery'){
+      if (usertype != 2){
+        return {}
+      }
+      form = (await db.collection('Member').where({}).orderBy('_time','desc').get()).data
+    } else if (event.type === 'ManagerFormsQuery'){
+      if (usertype != 2){
+        return {}
+      }
+      form = (await db.collection('Forms').where({}).orderBy('_time','desc').get()).data
     }
-    form = (await db.collection('Forms').where({_staffopenid: wxContext.OPENID}).orderBy('_time','desc').get()).data
-  } else if (event.type == 'ManagerMemberQuery'){
-    if (usertype != 2){
-      return {}
-    }
-    form = (await db.collection('Member').where({}).orderBy('_time','desc').get()).data
-  } else if (event.type == 'ManagerFormsQuery'){
-    if (usertype != 2){
-      return {}
-    }
-    form = (await db.collection('Forms').where({}).orderBy('_time','desc').get()).data
   }
   return form
 }
