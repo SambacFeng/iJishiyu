70dfac24ea639e1c30576f94fca40e4dceaa666b
SambacFeng <feng20000310@qq.com>
2021-09-01 18:48:41 +0800

    用户首页增加报名按钮

--- pages/userIndex/userIndex.js
+++ pages/userIndex/userIndex.js
@@ -5,14 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isRecruiting: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const db = wx.cloud.database()
   },
 
   /**
